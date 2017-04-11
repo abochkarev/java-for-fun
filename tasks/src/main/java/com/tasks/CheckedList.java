6e077145151edf0e6f4f817572abfbf68b8caf5c
Alexander Bochkarev <abochkarev@mirantis.com>
2017-04-11 14:57:06 +0400

    Add a checked list example.

---
+++ tasks/src/main/java/com/tasks/CheckedList.java
@@ -0,0 +1,27 @@
+package com.tasks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Dog {}
+class Cat {}
+
+public class CheckedList {
+
+    private static void insertCat(List probablyDogs, Cat cat) {
+        probablyDogs.add(cat);
+    }
+
+    public static void main(String []args) {
+        List<Dog> dogs = new ArrayList<>();
+        insertCat(dogs, new Cat());
+
+        List<Dog> dogs1 = Collections.checkedList(new ArrayList<>(), Dog.class);
+        try {
+            insertCat(dogs1, new Cat());
+        } catch (ClassCastException e) {
+            System.out.println(String.format("Unable to insert a Cat object to a Dogs list: %s", e));
+        }
+    }
+}
