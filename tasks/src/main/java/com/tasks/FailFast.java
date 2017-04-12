1ec149b4a03d1838ef146d461d3fe1fd1a623159
Alexander Bochkarev <abochkarev@mirantis.com>
2017-04-12 15:44:08 +0400

    Add a simple fail fast example.

---
+++ tasks/src/main/java/com/tasks/FailFast.java
@@ -0,0 +1,32 @@
+package com.tasks;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class FailFast {
+
+    public static void main(String []args) {
+        List<String> strings = new ArrayList<>(Arrays.asList("Hello ", "World "));
+        Iterator<String> iterator = strings.iterator();
+        strings.add("!");
+        try {
+            while (iterator.hasNext()) {
+                iterator.next();
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println("Unable to read a value from the list");
+        }
+
+        strings = new CopyOnWriteArrayList<>(Arrays.asList("World ", "Hello "));
+        iterator = strings.iterator();
+        strings.add("!");
+        try {
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println("Unable to read a value from the list");
+        }
+
+    }
+}
